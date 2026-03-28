56180fd776c28b0c5903e213e3ade78b7dacaa7a
Vaishali-Mankar <128403871+Vaishali-Mankar@users.noreply.github.com>
2026-03-28 14:12:46 +0530

    Rename compute instance from 'my-instance8' to 'my-instance9'

--- compute/main.tf
+++ compute/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "my-instance8"
+  name         = "my-instance9"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
 
