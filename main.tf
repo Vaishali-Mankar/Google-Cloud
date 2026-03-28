bb17b4adb6d0f14b0de65553a0639ee5faa46e5e
Vaishali-Mankar <128403871+Vaishali-Mankar@users.noreply.github.com>
2026-03-28 12:45:19 +0530

    Add Google Compute Instance resource configuration

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      
+    }
+  }
+
+ 
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
