bf688c38e0d40c8ef4994b527393fec2bae4ebfe
Vaishali-Mankar <128403871+Vaishali-Mankar@users.noreply.github.com>
2026-03-28 13:13:53 +0530

    Rename compute instance from 'my-instance2' to 'my-instance1'

--- compute/main.tf
+++ compute/main.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "default" {
-  name         = "my-instance2"
+  name         = "my-instance1"
   machine_type = "n2-standard-2"
   zone         = "us-central1-a"
 
